31da56c01994ba5d45eee92fee54a1d996aea208
Taylor <taylor.hackbart@du.edu>
2020-12-16 11:28:26 -0800

    nodes

--- client/src/pages/Graphs/Graphs.js
+++ client/src/pages/Graphs/Graphs.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 // import API from "../../utils/API"
 // import {Input, FormBtn} from "../../components/Form"
 import Search from "../../components/Search"
+import {DateRange} from "react-date-range"
 // const URL = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=ddb7dc274b3d157577d5acb1cd78e0a6`
 
 function Graphs(){
@@ -18,11 +19,13 @@ function Graphs(){
       console.log(data.coord)
       const lng = (data.coord.lon)
       const lat = (data.coord.lat)
-      fetch(`https://api.ipgeolocation.io/astronomy?apiKey=eaae8bc7476c441fab504887b3a7c3a8&lat=${lat}&long=${lng}`)
+      const date = 2020-11-11 
+      fetch(`https://api.ipgeolocation.io/astronomy?apiKey=eaae8bc7476c441fab504887b3a7c3a8&lat=${lat}&long=${lng}&date=2020-11-11`)
       .then(res => {
         res.json()
         .then(newData=> {
           console.log(newData)
+
         })
       })
     })
